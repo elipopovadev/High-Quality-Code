9bb6656f8272beba7a2544a0e4f658587950d716
Elena Popova <elipopovadev@gmail.com>
2021-06-07 20:35:41 +0200

    1.PrintDetails

---
+++ DesignPatterns-CreationalPatterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Singleton
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = new Singleton(); // only one object invoked with different print details
+            s1.PrintDetails("This is first message");
+            s1.PrintDetails("This is second message");
+        }
+    }
+}
---
+++ DesignPatterns-CreationalPatterns/Singleton/Singleton/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton
+{
+   public class Singleton
+    {
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
