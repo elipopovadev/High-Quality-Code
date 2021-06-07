6bc198f4d56531ecd8a6daa5012d315c7f133734
Elena Popova <elipopovadev@gmail.com>
2021-06-07 23:03:05 +0200

    3.SingletonWithGetInstance

---
+++ DesignPatterns-CreationalPatterns/Singleton/Singleton-withGetInstance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Singleton_withGetInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromEmployee = Singleton.GetInstance;
+            fromEmployee.PrintDetails("Message from employee");
+
+            Singleton fromMentor = Singleton.GetInstance;
+            fromMentor.PrintDetails("Message from mentor");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("Message from student");
+        }
+    }
+}
---
+++ DesignPatterns-CreationalPatterns/Singleton/Singleton-withGetInstance/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Singleton_withGetInstance
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+
+        private Singleton() // always is private
+        {
+
+        }
+
+        public static Singleton GetInstance // property with getter
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton(); // the instance is created only once
+                }
+
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
