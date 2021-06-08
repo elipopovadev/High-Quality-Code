1f92cf8663d29dd0e93e45abdc8aba5be3c7e863
Elena Popova <elipopovadev@gmail.com>
2021-06-08 11:50:48 +0200

    EagerInitialization

---
+++ DesignPatterns-CreationalPatterns/Singleton/Singleton-LazyInitialization/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Singleton_EagerInitialization
+{
+   public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
