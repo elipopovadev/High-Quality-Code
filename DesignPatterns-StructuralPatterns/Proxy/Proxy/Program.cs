0b5cb2bc62f21f6796cefcf3a69514f091784397
Elena Popova <elipopovadev@gmail.com>
2021-06-18 12:10:13 +0200

    UnderstandingProxyDesignPattern

--- DesignPatterns-StructuralPatterns/Proxy/Proxy/Program.cs
+++ DesignPatterns-StructuralPatterns/Proxy/Proxy/Program.cs
@@ -7,11 +7,11 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
-            Employee emp1 = new Employee("Anurag", "Anurag123", "Developer");
+            Employee emp1 = new Employee("Anurag", "Anurag123", "Developer"); // Dev can't access this shared folder
             ProxySharedFolder proxy = new ProxySharedFolder(emp1);
             proxy.PerformReadWriteOperations();
             Console.WriteLine();
-            Employee emp2 = new Employee("Elena", "Elena123", "Manager");
+            Employee emp2 = new Employee("Elena", "Elena123", "Manager"); //  Manager and CEO can access this shared folder 
             ProxySharedFolder proxy2 = new ProxySharedFolder(emp2);
             proxy2.PerformReadWriteOperations();
         }
