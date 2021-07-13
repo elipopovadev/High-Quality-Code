2ebe9e59686ea9c363b2d4648a15e0b1137bd78b
Elena Popova <elipopovadev@gmail.com>
2021-07-13 21:47:05 +0200

    FlyweightHomework

--- DesignPatterns-StructuralPatterns/Flyweight/FlyweightHomework/FlyweightFactory/BodyFactory.cs
+++ DesignPatterns-StructuralPatterns/Flyweight/FlyweightHomework/FlyweightFactory/BodyFactory.cs
@@ -8,7 +8,7 @@ namespace FlyweightHomework.FlyweightFactory
     public static class BodyFactory
     {
         // dictionary collection object which will act as a cache for our application
-        private static Dictionary<string, IBody> modelBodyDictionary = new Dictionary<string, IBody>();
+        private static Dictionary<string, IBody> modelBodyTypeDictionary = new Dictionary<string, IBody>();
 
         public static IBody GetCar(string model)
         {
@@ -16,29 +16,29 @@ namespace FlyweightHomework.FlyweightFactory
             
             if (model == nameof(VWGolf))
             {
-                if (modelBodyDictionary.ContainsKey("VWGolf"))
+                if (modelBodyTypeDictionary.ContainsKey("VWGolf"))
                 {
-                    bodyType = modelBodyDictionary[model];
+                    bodyType = modelBodyTypeDictionary[model];
                 }
 
                 else
                 {
                     bodyType =  new VWGolf();
-                    modelBodyDictionary.Add("VWGolf", bodyType); // Creating VWGolf object without any body                
+                    modelBodyTypeDictionary.Add("VWGolf", bodyType); // Creating VWGolf object without any body                
                 }
             }
 
             else if (model == nameof(ToyotaAvensis))
             {
-                if (modelBodyDictionary.ContainsKey(model))
+                if (modelBodyTypeDictionary.ContainsKey(model))
                 {
-                    bodyType = modelBodyDictionary[model];
+                    bodyType = modelBodyTypeDictionary[model];
                 }
 
                 else
                 {
                     bodyType = new ToyotaAvensis();
-                    modelBodyDictionary.Add("ToyotaAvensis", bodyType); // Creating ToyotaAvensis object without any body                
+                    modelBodyTypeDictionary.Add("ToyotaAvensis", bodyType); // Creating ToyotaAvensis object without any body                
                 }
             }
 
