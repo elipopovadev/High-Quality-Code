4c475deb4b5cbd07a07b1a82dda2e3db376e5737
Elena Popova <elipopovadev@gmail.com>
2021-07-21 10:32:29 +0200

    Iterator

---
+++ DesignPatterns-BehavioralPatterns/Iterator/Iterator/Numbers.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+   public class Numbers : IEnumerable<int> // IEnumerable<int>
+    {
+        private readonly int[] numbers;
+
+        public Numbers(int[] numbers)
+        {
+            this.numbers = numbers;
+        }
+        public int Count => this.numbers.Length;
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return new NumbersEnumerator(this.numbers);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator(); // always return this.GetEnumerator()
+        }
+
+
+        private class NumbersEnumerator : IEnumerator<int> // IEnumerator<int>
+        {
+            private readonly int[] numbers;
+            private int currentIndex = -1; // add currentIndex
+
+            public NumbersEnumerator(int[] numbers)
+            {
+                this.Reset(); // add Reset()
+                this.numbers = numbers;
+            }
+
+            public int Current => this.numbers[currentIndex];
+
+            object IEnumerator.Current => this.Current; // always return this.Current
+
+            public bool MoveNext()
+            {
+                this.currentIndex++;
+                if (this.currentIndex >= this.numbers.Length)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+
+            public void Dispose()
+            {
+
+            }
+        }
+    }
+}
---
+++ DesignPatterns-BehavioralPatterns/Iterator/Iterator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new Numbers(new int[] { 1, 2, 3, 4, 5 });
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
