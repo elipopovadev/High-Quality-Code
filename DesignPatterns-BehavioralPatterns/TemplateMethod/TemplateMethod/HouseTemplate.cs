70748fea8c8926ebee909348895f7dc846300fa0
Elena Popova <elipopovadev@gmail.com>
2021-07-24 15:24:20 +0200

    TemplateMethod

---
+++ DesignPatterns-BehavioralPatterns/TemplateMethod/TemplateMethod/HouseTemplate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+   public abstract class HouseTemplate // always is abstract class
+    {
+        // Template method defines the sequence for building a house
+        public void BuildHouse()
+        {
+            BuildFoundation();
+            BuildPillars();
+            BuildWalls();
+            BuildWindows();
+            Console.WriteLine("House is built");
+        }
+
+        // Methods to be implemented by subclasses
+        protected abstract void BuildFoundation(); // abstract
+        protected abstract void BuildPillars(); // abstract
+        protected abstract void BuildWalls(); // abstract
+        protected abstract void BuildWindows(); // abstract
+    }
+}
---
+++ DesignPatterns-BehavioralPatterns/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build a Concrete House: ");
+            HouseTemplate houseTemplate = new ConcreteHouse();
+            houseTemplate.BuildHouse();
+
+            Console.WriteLine();
+            Console.WriteLine("Build a Wooden House: ");
+            houseTemplate = new WoodenHouse();
+            houseTemplate.BuildHouse();
+        }
+    }
+}
